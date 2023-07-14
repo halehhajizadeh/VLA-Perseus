145fc7f9559ee3af3209ecfdad1da75bb5f1b448
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-07-14 14:21:32 -0600

    changed all msfilenames with importing config

---
+++ scripts/configs.py
@@ -0,0 +1,8 @@
+path = '../data/19B-053_2019_12_15_T07_36_56.546/products/'
+msfilename = path + '19B-053.sb37264871.eb37595549.58832.22860825231_calibrated.ms'
+
+refant_name = 'ea08'
+phase_center = "J2000 03:32:54.609000 +31.11.15.01999"
+thresh = '1e-4'
+nit = 20000
+threedigits ='546'
