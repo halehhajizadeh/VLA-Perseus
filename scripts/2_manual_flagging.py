b501fa50bdaf23e6af128edf13545187d575b6a7
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-07-17 19:51:51 -0600

    add if to specify the data

--- scripts/2_manual_flagging.py
+++ scripts/2_manual_flagging.py
@@ -1,33 +1,38 @@
 import sys
 sys.path.append('.')
-from configs import msfilename
+from configs import msfilename, threedigits
 
-flagdata(
-vis=msfilename, 
-mode='manual',
-spw='0:19~32'
-)
+if threedigits=='546':
+    flagdata(
+    vis=msfilename, 
+    mode='manual',
+    spw='0:19~32'
+    )
 
-flagdata( 
-vis=msfilename, 
-mode='manual', 
-spw='3:0~5'
-)
+    flagdata( 
+    vis=msfilename, 
+    mode='manual', 
+    spw='3:0~5'
+    )
 
-flagdata( 
-vis=msfilename, 
-mode='manual', 
-spw='0:0~8'
-)
+    flagdata( 
+    vis=msfilename, 
+    mode='manual', 
+    spw='0:0~8'
+    )
 
-flagdata( 
-vis=msfilename, 
-mode='manual', 
-spw='9,11,12,7'
-)
+    flagdata( 
+    vis=msfilename, 
+    mode='manual', 
+    spw='9,11,12,7'
+    )
 
-flagdata( 
-vis=msfilename, 
-mode='manual', 
-spw='15:12~15'
-)
+    flagdata( 
+    vis=msfilename, 
+    mode='manual', 
+    spw='15:12~15'
+    )
+#----------------------------------------------------------------
+if threedigits=='755':
+
+
