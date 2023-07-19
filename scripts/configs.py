254b7d14f37414387d815fd43c56a523974459b6
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-07-19 01:20:27 -0600

    refant updated

--- scripts/configs.py
+++ scripts/configs.py
@@ -1,7 +1,7 @@
 path = '../data/19B-053_2019_12_16_T08_12_56.775/products/'
 msfilename = path + '19B-053.sb37264871.eb37596495.58833.2500384375_calibrated.ms'
 
-refant_name = 'ea08'
+refant_name = 'ea24'
 phase_center = 'J2000 03:32:54.609000 +31.11.15.01999'
 thresh = '1e-4'
 nit = 10000
