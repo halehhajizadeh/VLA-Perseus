6a0c0e688c13893bb898c952a5d21f3f49d15ddd
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-07-19 02:48:26 -0600

    phase center added

--- scripts/configs.py
+++ scripts/configs.py
@@ -2,7 +2,7 @@ path = '../data/19B-053_2019_12_16_T08_12_56.775/products/'
 msfilename = path + '19B-053.sb37264871.eb37596495.58833.2500384375_calibrated.ms'
 
 refant_name = 'ea24'
-phase_center = 'J2000 03:32:54.609000 +31.11.15.01999'
+phase_center = 'ICRS 05:42:36.1382 +049.51.07.242'
 thresh = '1e-4'
 nit = 10000
 threedigits ='755'
