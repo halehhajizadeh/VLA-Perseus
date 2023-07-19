cdb68f0c10e26fbf6997a1d9806bf95acd97bb56
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-07-19 02:18:55 -0600

    refantadded

--- scripts/7_polcal_xf.py
+++ scripts/7_polcal_xf.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('.')
-from configs import msfilename
+from configs import msfilename, refant_name
 
 polcal(
     vis=msfilename,
@@ -10,7 +10,7 @@ polcal(
     solint='inf,2MHz',
     combine='scan',
     poltype='Xf',
-    refant='ea08',
+    refant=refant_name,
     gaintable=[msfilename+'.Kcross_sbd',msfilename+'.Df_sbd'],
     gainfield=['',''],
     append=False)
