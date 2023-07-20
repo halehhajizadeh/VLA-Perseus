1bd4128d5c9d6c7e0be8c89e28a55a15dc6ee169
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-07-20 12:57:40 -0600

    made this script to make images of all measurement sets

---
+++ scripts/bigMosaic.py
@@ -0,0 +1,27 @@
+import os
+import tarfile
+import shutil
+
+working_directory = '/stash/users/pjaganna/evla/19B-053/'
+
+def find_ms_folder(directory, startswith='19B-053', endswith=''):
+    """
+    Finds names of ms files in a directroy.
+
+    directory (str): The directory to search
+    startswith (str): The beginning of the file to searchs
+    endswith (str): The end of the file to search
+
+    Returns:
+    str : An array including the name of the ms files found.
+    """
+    folders_list = []
+    for file in os.listdir(directory):
+        if file.startswith(startswith):
+            if file.endswith(endswith):
+                folders_list.append(os.path.join(directory, file))                
+    return(folders_list)
+
+
+mslist = find_ms_folder (working_directory, startswith='19B-053', endswith='/')
+print(mslist)
