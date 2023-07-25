5463a215f757aecc7ba798528c67bd3b4f1bd763
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-07-25 01:45:59 -0600

    done

--- scripts/9_plot_field_location.py
+++ scripts/9_plot_field_location.py
@@ -1,9 +1,8 @@
 import sys
 sys.path.append('.')
-from configs import  threedigits
 import matplotlib.pyplot as plt
 import numpy as np
-
+import os
 
 from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
@@ -11,66 +10,86 @@ rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 #rc('font',**{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
 
+working_directory = '../data'
+def find_ms_folder(directory, startswith='19B-053', endswith=''):
+    """
+    Finds names of ms files in a directroy.
 
-#This file is a text file with each line being the name of your field, the RA as 'hh:mm:ss.sss' and Dec as 'dd.mm.ss.ssss' of the center of the source separated by tabs, I have an example if you need it. The data is from listobs
-f='field_RADec_'+str(threedigits)+'.txt'
-l=open(f)
-data=l.readlines()
+    directory (str): The directory to search
+    startswith (str): The beginning of the file to searchs
+    endswith (str): The end of the file to search
 
-ID=[]
-ra_raw=[]
-dec_raw=[]
+    Returns:
+    str : An array including the name of the ms files found.
+    """
+    folders_list = []
+    for file in os.listdir(directory):
+        if file.startswith(startswith):
+            if file.endswith(endswith):
+                folders_list.append(os.path.join(directory, file))                
+    return(folders_list)
 
-for d in data:
-    dat_split=d.split()
-    ID.append(dat_split[0])
-    ra_raw.append(dat_split[1])
-    dec_raw.append(dat_split[2].split('\n')[0])
-print(ID)
-print(ra_raw)
-print(dec_raw)
-ra_deg=[]
-for r in ra_raw:
-    ra_split=r.split(':')
-    ra_h=ra_split[0]
-    ra_m=ra_split[1]
-    ra_s=ra_split[2]
-    if float(ra_h) > 0:
-        ra=(float(ra_h)+float(ra_m)/60+float(ra_s)/3600)*15
-    else:
-        ra=(float(ra_h)-float(ra_m)/60-float(ra_s)/3600)*15
-    ra_deg.append(ra)
-    
-dec_deg=[]
-for c in dec_raw:
-    dec_split=c.split('.', 2)
-    dec_d=dec_split[0]
-    dec_m=dec_split[1]
-    dec_s=dec_split[2]
-    if float(dec_d) > 0:
-        dec=float(dec_d)+float(dec_m)/60+float(dec_s)/3600
-    else:
-        dec=float(dec_d)-float(dec_m)/60-float(dec_s)/3600
-    dec_deg.append(dec)
+mslist = find_ms_folder (working_directory, startswith='19B-053', endswith='')
+print(mslist)
+
+for ms_folder in mslist:
+    #This file is a text file with each line being the name of your field, the RA as 'hh:mm:ss.sss' and Dec as 'dd.mm.ss.ssss' of the center of the source separated by tabs, I have an example if you need it. The data is from listobs
+    f='./phasecenter/'+str(ms_folder.split('/')[-1])+'_radecs.txt'
+    l=open(f)
+    data=l.readlines()
+
+    ID=[]
+    ra_raw=[]
+    dec_raw=[]
+
+    for d in data:
+        dat_split=d.split()
+        ID.append(dat_split[0])
+        ra_raw.append(dat_split[1])
+        dec_raw.append(dat_split[2].split('\n')[0])
+    print(ID)
+    print(ra_raw)
+    print(dec_raw)
+    ra_deg=[]
+    for r in ra_raw:
+        ra_split=r.split(':')
+        ra_h=ra_split[0]
+        ra_m=ra_split[1]
+        ra_s=ra_split[2]
+        if float(ra_h) > 0:
+            ra=(float(ra_h)+float(ra_m)/60+float(ra_s)/3600)*15
+        else:
+            ra=(float(ra_h)-float(ra_m)/60-float(ra_s)/3600)*15
+        ra_deg.append(ra)
+        
+    dec_deg=[]
+    for c in dec_raw:
+        dec_split=c.split('.', 2)
+        dec_d=dec_split[0]
+        dec_m=dec_split[1]
+        dec_s=dec_split[2]
+        if float(dec_d) > 0:
+            dec=float(dec_d)+float(dec_m)/60+float(dec_s)/3600
+        else:
+            dec=float(dec_d)-float(dec_m)/60-float(dec_s)/3600
+        dec_deg.append(dec)
 
 
-plt.rc('text',usetex=True)
-plt.rc('font',family='serif')
+    plt.rc('text',usetex=True)
+    plt.rc('font',family='serif')
 
-plt.figure(1)
+    plt.figure(1)
 
-plt.subplot(111)
+    plt.subplot(111)
 
-plt.plot(ra_deg, dec_deg, 'b.')
-plt.xlabel(r'RA (deg)',fontsize=15)
-plt.ylabel(r'DEC (deg)',fontsize=15)
-# plt.xlim(265.5,262.5)
-plt.tick_params(axis='x',labelsize=14)
-plt.tick_params(axis='y',labelsize=14)
+    plt.plot(ra_deg, dec_deg, 'b.')
+    plt.xlabel(r'RA (deg)',fontsize=15)
+    plt.ylabel(r'DEC (deg)',fontsize=15)
+    # plt.xlim(265.5,262.5)
+    plt.tick_params(axis='x',labelsize=14)
+    plt.tick_params(axis='y',labelsize=14)
 
-for i, txt in enumerate(ID):
-    plt.annotate(txt, (ra_deg[i], dec_deg[i]))
+    for i, txt in enumerate(ID):
+        plt.annotate(txt, (ra_deg[i], dec_deg[i]))
 
-plt.savefig('Fields.png')
-
-plt.show()
+    plt.savefig('./phasecenter/'+str(ms_folder.split('/')[-1])+'Fields.png')
