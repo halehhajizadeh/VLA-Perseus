44bc58047c7d2f9f1736d3d37d58ba0d9ce6c9b6
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-08-07 23:57:29 -0600

    calibrator rflag added

--- scripts/1_initialize_data.py
+++ scripts/1_initialize_data.py
@@ -190,6 +190,21 @@ flagdata(vis=filename,
          flagbackup=True, 
          savepars=True)
 
+flagdata(vis=filename,
+         mode='rflag', 
+         correlation='RL, LR', 
+         intent='*CALIBRATE*',
+         datacolumn='corrected', 
+         ntime='scan', 
+         combinescans=False,
+         extendflags=False, 
+         winsize=3, 
+         timedevscale=4.0, 
+         freqdevscale=4.0,
+         action='apply', 
+         flagbackup=True, 
+         savepars=True)
+
 
 flagdata(vis= filename,
          mode='rflag', 
