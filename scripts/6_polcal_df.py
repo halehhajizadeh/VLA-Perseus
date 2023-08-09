d7f66f42f96fe439ca4eeaef6f524cb11496b960
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-08-09 00:55:54 -0600

    some update

--- scripts/6_polcal_df.py
+++ scripts/6_polcal_df.py
@@ -2,9 +2,11 @@ import sys
 sys.path.append('.')
 from configs import msfilename, refant_name
 
+#if we have an instrumental polarization calibrator that we know is unpolarized, we run polcal with poltype=’Df
+
 polcal(vis=msfilename,
        caltable=msfilename+'.Df_sbd',
-       field='0',
+       field='0542+498=3C147',
        scan = '2,3',
        spw='',
        refant=refant_name,
