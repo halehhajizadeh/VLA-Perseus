d085434094419fdc1abad1a40b6ccb3e82e77d77
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-08-30 13:02:41 -0600

    removed /

--- scripts/configs.py
+++ scripts/configs.py
@@ -1,6 +1,6 @@
 #546
 path = '../data/19B-053_2019_12_15_T07_36_56.546/products/'
-msfilename = path + '19B-053.sb37264871.eb37595549.58832.22860825231_calibrated.ms/'
+msfilename = path + '19B-053.sb37264871.eb37595549.58832.22860825231_calibrated.ms'
 
 refant_name = 'ea24'
 phase_center = 'J2000 03:32:04.530001 +31.05.04.00000'
