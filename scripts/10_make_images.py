6e0469b5e18948e9110813c7641c9d5c90d6d8f3
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-09-01 14:46:59 -0600

    parallel false

--- scripts/10_make_images.py
+++ scripts/10_make_images.py
@@ -60,7 +60,7 @@ for stok in stokes1:
                 restart=True,
                 calcres=True,
                 calcpsf=True,
-                parallel=True,
+                parallel=False,
                 interactive=False)
             toc = time.time()
             print(f"stokes: {stok}, s: {s}, channel: {channel} is finished!")
