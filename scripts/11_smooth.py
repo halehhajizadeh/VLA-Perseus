48500116e92b9ae4472736739f83293bbedf38bb
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-09-06 16:12:36 -0600

    up

--- scripts/11_smooth.py
+++ scripts/11_smooth.py
@@ -28,7 +28,7 @@ for stok in stokes:
         for channel in channels:  
             imsmooth(imagename = path+"/Images/img"+str(nit)+"/tclean/"+threedigits+"-spw"+str(s)+'-'+ str(channel)+"-2.5arcsec-nit"+str(nit)+"-"+str(thresh)+"-"+str(stok)+'.image',
                     targetres = True,
-                    major = '55arcsec',
+                    major = '60arcsec',
                     minor ='50arcsec',
                     pa='0.0deg',
                     outfile = path+"/Images/img"+str(nit)+"/smo/"+threedigits+"-spw"+str(s)+'-'+ str(channel)+"-2.5arcsec-nit"+str(nit)+"-"+str(thresh)+"-"+str(stok)+'.image.smo',
