7a3974d2921c7584d79c094414c0afe35eee7ec0
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-09-06 23:52:15 -0600

    up

--- scripts/12_exportfits.py
+++ scripts/12_exportfits.py
@@ -3,7 +3,7 @@ sys.path.append('.')
 from configs import path, thresh, nit, threedigits
 import time
 
-spw = [0, 2, 3 , 4, 5, 6, 8, 10, 15, 16, 17]
+spw = [ 2, 3 , 4, 5, 6, 8, 15, 16, 17]
 stokes = [
         'I',
         'Q',
@@ -30,5 +30,4 @@ for stok in stokes:
 
 toc = time.time()
 print(f"Finshed the exporting process in {round((toc-tic)/60)} minutes")       
-
 
