f88bfdfa6a20a16d1e0e493994dc4d6aad1ec117
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-09-07 12:28:41 -0600

    read list of images form the directory

--- scripts/13_list_images.py
+++ scripts/13_list_images.py
@@ -11,25 +11,24 @@ for file in file_list:
     file_list_total.append(file)
 print (file_list_total)
 
-# spw = [9,10,11,12,13,14,15,16,17,0,1,2,3,4,5,6,7,8]
-# # existing_spw = [2, 3 , 4, 5, 6, 8, 10, 15, 16, 17]
-# # non_spw = [0, 1, 7, 9, 11, 12, 13, 14]
-# channels = ['00~07', '08~15', '16~23', '24~31', '32~39', '40~47', '48~55', '56~63']
+spw = [9,10,11,12,13,14,15,16,17,0,1,2,3,4,5,6,7,8]
+channels = ['00~07', '08~15', '16~23', '24~31', '32~39', '40~47', '48~55', '56~63']
 
-# stokes = [
-#         'I',
-#         'Q',
-#         'U'
-#           ]
+stokes = [
+        'I',
+        'Q',
+        'U'
+          ]
 
-# for stok in stokes:
-#     files_list = []
-#     for s in spw:
-#         for channel in channels:
-#             if s in existing_spw:
-#                 files_list.append(path+"/Images/img"+str(nit)+"/fits/"+threedigits+"-spw"+str(s)+'-'+ str(channel)+"-2.5arcsec-nit"+str(nit)+"-"+str(thresh)+"-"+str(stok)+'.image.smo.fits')
-#             elif s in non_spw:
-#                 files_list.append(path+"/Images/img"+str(nit)+"/fits/empty_channel.fits")    
+for stok in stokes:
+    files_list = []
+    for s in spw:
+        for channel in channels:
+            file_name = path+"/Images/img"+str(nit)+"/fits/"+threedigits+"-spw"+str(s)+'-'+ str(channel)+"-2.5arcsec-nit"+str(nit)+"-"+str(thresh)+"-"+str(stok)+'.image.smo.fits'
+            if file_name in file_list_total:
+                files_list.append(file_name)
+            elif file_name not in file_list_total:
+                files_list.append(path+"/Images/img"+str(nit)+"/fits/empty_channel.fits")    
         
-#     np.savetxt(path+'/Images/img'+str(nit)+'/stokes' + str(stok) +'.txt', files_list, fmt ='%s')            
+    np.savetxt(path+'/Images/img'+str(nit)+'/stokes' + str(stok) +'.txt', files_list, fmt ='%s')            
             
