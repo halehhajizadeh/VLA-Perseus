2a265a36da2aa99a4ecdb0d6fc69a941f2c78027
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-09-07 12:09:20 -0600

    up

--- scripts/13_list_images.py
+++ scripts/13_list_images.py
@@ -2,26 +2,30 @@ import sys
 sys.path.append('.')
 from configs import path, thresh, nit, threedigits
 import numpy as np
+import os 
 
-spw = [9,10,11,12,13,14,15,16,17,0,1,2,3,4,5,6,7,8]
-existing_spw = [0, 2, 3 , 4, 5, 6, 8, 15, 16, 17]
-non_spw = [1, 7, 9, 10, 11, 12, 13, 14]
-channels = ['00~07', '08~15', '16~23', '24~31', '32~39', '40~47', '48~55', '56~63']
+file_list = [file for file in os.listdir(path+"/Images/img"+str(nit)+"/fits/") if os.path.isfile(os.path.join(path+"/Images/img"+str(nit)+"/fits/", file))]
+print (file_list)
 
-stokes = [
-        'I',
-        'Q',
-        'U'
-          ]
+# spw = [9,10,11,12,13,14,15,16,17,0,1,2,3,4,5,6,7,8]
+# # existing_spw = [2, 3 , 4, 5, 6, 8, 10, 15, 16, 17]
+# # non_spw = [0, 1, 7, 9, 11, 12, 13, 14]
+# channels = ['00~07', '08~15', '16~23', '24~31', '32~39', '40~47', '48~55', '56~63']
 
-for stok in stokes:
-    files_list = []
-    for s in spw:
-        for channel in channels:
-            if s in existing_spw:
-                files_list.append(path+"/Images/img"+str(nit)+"/fits/"+threedigits+"-spw"+str(s)+'-'+ str(channel)+"-2.5arcsec-nit"+str(nit)+"-"+str(thresh)+"-"+str(stok)+'.image.smo.fits')
-            elif s in non_spw:
-                files_list.append(path+"/Images/img"+str(nit)+"/fits/empty_channel.fits")    
+# stokes = [
+#         'I',
+#         'Q',
+#         'U'
+#           ]
+
+# for stok in stokes:
+#     files_list = []
+#     for s in spw:
+#         for channel in channels:
+#             if s in existing_spw:
+#                 files_list.append(path+"/Images/img"+str(nit)+"/fits/"+threedigits+"-spw"+str(s)+'-'+ str(channel)+"-2.5arcsec-nit"+str(nit)+"-"+str(thresh)+"-"+str(stok)+'.image.smo.fits')
+#             elif s in non_spw:
+#                 files_list.append(path+"/Images/img"+str(nit)+"/fits/empty_channel.fits")    
         
-    np.savetxt(path+'/Images/img'+str(nit)+'/stokes' + str(stok) +'.txt', files_list, fmt ='%s')            
+#     np.savetxt(path+'/Images/img'+str(nit)+'/stokes' + str(stok) +'.txt', files_list, fmt ='%s')            
             
