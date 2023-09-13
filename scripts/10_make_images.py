2457788f12b06081aad4b02ec2a0c5fe1c836e96
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-09-12 22:27:33 -0600

    up

--- scripts/10_make_images.py
+++ scripts/10_make_images.py
@@ -4,7 +4,7 @@ from configs import path, phase_center, thresh, nit, threedigits
 import time
 
 filename = path+'/targets.ms'
-spw = [2, 3 , 4, 5, 6, 8, 10, 15, 16, 17]
+spw = [0, 2, 3 , 4, 5, 6, 8, 10, 15, 16, 17]
 stokes1 = [
         'I',
         'Q',
