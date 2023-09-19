d1e1c67994ec8319f3d2da2c8195d06c6da06446
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-09-19 17:44:00 -0600

    parallel true

--- scripts/10_make_images.py
+++ scripts/10_make_images.py
@@ -57,7 +57,7 @@ for stok in stokes1:
                     restart=True,
                     calcres=True,
                     calcpsf=True,
-                    parallel=False,
+                    parallel=True,
                     interactive=False)
         
             toc = time.time()
