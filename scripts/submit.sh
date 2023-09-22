c3ff071585561b9b5e91d6cc15671d7a7c88e494
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-09-22 13:54:25 -0600

    fixed the path bug

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -8,9 +8,10 @@
 # SBATCH --export=ALL                          # Export all environment variables to job
 # SBATCH --chdir=/lustre/aoc/observers/nm-12934/VLA_Perseus # Working directory
 # SBATCH --time=8-0:0:0                        # Request 8days
-# SBATCH --mem=256G                            # Memory for the whole job
+# SBATCH --mem=128G                            # Memory for the whole job
 # SBATCH --nodes=1                             # Request 1 node
 # SBATCH --ntasks-per-node=8                   # Request 8 cores
 
 CASAPATH=/home/casa/packages/RHEL7/release/current # Use a specific version of CASA
-xvfb-run -d mpicasa ${CASAPATH}/bin/casa --nogui -c /lustre/aoc/observers/nm-12934/VLA-Perseus/scripts/10_make_images.py >>file.txt 2>&1
+# xvfb-run -d mpicasa ${CASAPATH}/bin/casa --nogui -c /lustre/aoc/observers/nm-12934/VLA-Perseus/scripts/10_make_images.py >>file.txt 2>&1
+xvfb-run -d ${CASAPATH}/bin/mpicasa ${CASAPATH}/bin/casa --nogui -c /lustre/aoc/observers/nm-12934/VLA-Perseus/scripts/10_make_images.py >>file.txt 2>&1
