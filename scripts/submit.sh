598c3fe1c0b0eefe2636cdeca3fc3c36e37e4504
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-09-23 00:00:11 -0600

    deleted

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Set PBS Directives
-# Lines starting with "#SBATCH", before any shell commands are
-# interpreted as command line arguments to sbatch.
-# Don't put any commands before the #SBATCH directives or they won't work.
-
 # SBATCH --export=ALL                          # Export all environment variables to job
 # SBATCH --chdir=/lustre/aoc/observers/nm-12934/VLA_Perseus # Working directory
 # SBATCH --time=8-0:0:0                        # Request 8days
