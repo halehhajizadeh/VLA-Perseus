0e4bca4567515589c488fcf7a59b987b54d0b4ad
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-10-20 17:39:15 -0600

    "

--- scripts/9_split.py
+++ scripts/9_split.py
@@ -8,6 +8,8 @@ directory_path = path + "/targets.ms"
 
 if os.path.exists(directory_path):
     shutil.rmtree(directory_path)
+
+if os.path.exists(directory_path):
     shutil.rmtree(directory_path+".flagversions")
 
 print('splitting is starting...')
