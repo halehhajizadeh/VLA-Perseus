e3aa120f5841ef766434dd8920eb4cc5b00da67a
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2023-10-20 17:19:22 -0600

    keep phase calibraotr

--- scripts/9_split.py
+++ scripts/9_split.py
@@ -1,14 +1,18 @@
 import sys
 sys.path.append('.')
 from configs import msfilename, path
+import os
 
- 
+if os.path.exists(path+"/targets.ms"):
+    os.remove(path+"/targets.ms") 
+
 print('splitting is starting...')
 
 split(vis=msfilename,
       outputvis=path+'/targets.ms',
       datacolumn='corrected',
-      field='PER_FIELD_*')
+      field='PER_FIELD_*, J0336+3218'
+      )
 print('splitting targets is done!')
 
 
