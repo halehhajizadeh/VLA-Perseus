cee08c98b83a709d6dabf8036d0d3102acf5ddd4
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-03-27 22:21:06 -0600

    path fixed

--- scripts/configs.py
+++ scripts/configs.py
@@ -1,5 +1,5 @@
 #546
-path = '../data/19B-053_2019_12_15_T07_36_56.546/products'
+path = '../data/03:32:04.530001_+31.05.04.00000/data/19B-053_2019_12_15_T07_36_56.546/products'
 msfilename = path + '/19B-053.sb37264871.eb37595549.58832.22860825231_calibrated.ms'
 
 refant_name = 'ea24'
