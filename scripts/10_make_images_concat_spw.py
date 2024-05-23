c353ec78d344bcf4bac0c9113c3e24fcb78dbbdb
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-05-23 12:41:09 -0600

    path corrected

--- scripts/10_make_images_concat_spw.py
+++ scripts/10_make_images_concat_spw.py
@@ -52,8 +52,6 @@ for s in spw:
     tic = time.time()
     print(f"Stokes: I, s: {s} is started ...")
 
-    img_filename = path + "/concat/total/Images/img" + str(nit) + "/tclean/" +  "4-spw" + str(s) + "-2.5arcsec-nit" + str(nit) + "-" + str(thresh) + '-awproject'
-
     tclean( vis=ms_file_list,
             field="PER_FIELD_*, J0336+3218",
             spw=str(s),
