58c984c0272e30af5ead7eeb96b42a8935745e03
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-06-04 12:38:58 -0600

    submit

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -11,4 +11,4 @@
 CASAPATH=/home/casa/packages/RHEL7/release # Use a specific version of CASA
 # CASAPATH=/opt/local/bin/
 
-xvfb-run -d ${CASAPATH}/current/bin/mpicasa ${CASAPATH}/current/bin/casa --nogui -c /lustre/aoc/observers/nm-12934/VLA-Perseus/scripts/10_make_images.py >>file.txt 2>&1
+xvfb-run -d ${CASAPATH}/current/bin/mpicasa ${CASAPATH}/current/bin/casa --nogui -c /lustre/aoc/observers/nm-12934/VLA-Perseus/scripts/10_make_images_concat_spw.py >>file.txt 2>&1
