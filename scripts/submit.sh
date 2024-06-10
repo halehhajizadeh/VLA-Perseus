a01f115381eed9905c6dddbda29c769a525e73ef
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-06-10 12:40:41 -0600

    update

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
-# SBATCH --export=ALL                          # Export all environment variables to job
-# SBATCH --chdir=/lustre/aoc/observers/nm-12934/VLA_Perseus/scripts # Working directory
-# SBATCH --time=8-0:0:0                        # Request 8days
-# SBATCH --mem=256G                            # Memory for the whole job
-# SBATCH --nodes=1                            # Request 1 node
-# SBATCH --ntasks-per-node=16                   # Request 8 cores
+#SBATCH --export=ALL
+#SBATCH --chdir=/lustre/aoc/observers/nm-12934/VLA_Perseus/scripts
+#SBATCH --time=8-0:0:0
+#SBATCH --mem=256G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
 
+CASAPATH=/home/casa/packages/RHEL7/release/current/bin/
+# CASAPATH=/opt/local/bin/
 
-CASAPATH=/home/casa/packages/RHEL7/release/current/bin/ # Use a specific version of CASA
+# Get the number of available cores on the node
+available_cores=$(sinfo -N -l | grep -m 1 "idle" | awk '{print $4}')
 
-xvfb-run -d ${CASAPATH}/mpicasa -n 16 ${CASAPATH}/casa --nogui --nologger --log2term /lustre/aoc/observers/nm-12934/VLA-Perseus/scripts/10_make_images_concat_spw.py >> file.txt 2>&1
+if [ "$available_cores" -lt 16 ]; then
+    echo "Not enough slots available, reducing the number of tasks to $available_cores"
+    ntasks=$available_cores
+else
+    ntasks=16
+fi
+
+# Run CASA with xvfb for virtual frame buffer support
+xvfb-run -d ${CASAPATH}/mpicasa -n $ntasks ${CASAPATH}/casa --nogui --nologger --log2term /lustre/aoc/observers/nm-12934/VLA-Perseus/scripts/10_make_images_concat_spw.py >> file.txt 2>&1
