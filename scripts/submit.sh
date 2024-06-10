7fa6a2ac54537dd681d577104c5d390f30cc6a3b
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-06-10 12:59:38 -0600

    update

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -5,10 +5,10 @@
 #SBATCH --time=8-0:0:0
 #SBATCH --mem=256G
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=8
 
 CASAPATH=/home/casa/packages/RHEL7/release/current/bin/
 
 
 # Run CASA with xvfb for virtual frame buffer support
-xvfb-run -d ${CASAPATH}/mpicasa -n $ntasks ${CASAPATH}/casa --nogui --nologger --log2term /lustre/aoc/observers/nm-12934/VLA-Perseus/scripts/10_make_images_concat_spw.py >> file.txt 2>&1
+xvfb-run -d ${CASAPATH}/mpicasa -n 8 $ntasks ${CASAPATH}/casa --nogui --nologger /lustre/aoc/observers/nm-12934/VLA-Perseus/scripts/10_make_images_concat_spw.py >> file.txt 2>&1
