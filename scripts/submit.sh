53dcdcd1dd5316b7f91cac8346e4f4e1297f0239
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-06-11 11:55:35 -0600

    update

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -4,10 +4,10 @@
 #SBATCH --chdir=/lustre/aoc/observers/nm-12934/VLA_Perseus/scripts # Working directory
 #SBATCH --time=8-0:0:0                        # Request 8 days
 #SBATCH --mem=256G                            # Memory for the whole job
-#SBATCH --nodes=8                            # Request 1 node
+#SBATCH --nodes=1                          # Request 1 node
 #SBATCH --ntasks-per-node=16                   # Request 8 cores (adjust if necessary)
 
 
 CASAPATH=/home/casa/packages/RHEL7/release/current/bin/ # Use a specific version of CASA
 
-xvfb-run -d ${CASAPATH}mpicasa -n 16 ${CASAPATH}casa -c --nogui /lustre/aoc/observers/nm-12934/VLA-Perseus/scripts/10_make_images_concat_spw.py >>file.txt 2>&1
+xvfb-run -d ${CASAPATH}mpicasa ${CASAPATH}casa --nogui -c /lustre/aoc/observers/nm-12934/VLA-Perseus/scripts/10_make_images_concat_spw.py >>file.txt 2>&1
