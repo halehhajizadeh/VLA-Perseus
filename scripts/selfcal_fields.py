c7ca37d62691d0e92f1485e8e76f0611e5b403ad
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-07-02 15:29:46 -0600

    selfcal fields

--- scripts/selfcal_fields.py
+++ scripts/selfcal_fields.py
@@ -1,207 +1,192 @@
+import sys
+sys.path.append('.')
+import time
+import os
+import numpy as np
 
 
-gaincal(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/', field='PER_FIELD_*', caltable='phase_120.cal',solint='120s',refant='ea16',calmode='p',gaintype='G')
-applycal(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/', field='PER_FIELD_*', gaintable=['phase_120.cal'], interp='linear')
+def find_calibrated_files(base_directory):
+    calibrated_files = []
 
-split(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/', field='PER_FIELD_*', datacolumn='corrected', outputvis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged_selfcaled1.ms/')
+    # Traverse through all directories and subdirectories
+    for root, dirs, files in os.walk(base_directory):
+        for directory in dirs:
+            if directory.startswith('19B-053'):
+                products_path = os.path.join(root, directory, 'products')
+                if os.path.exists(products_path):
+                    for file in os.listdir(products_path):
+                        if file.startswith('19B-053') and file.endswith('_calibrated_working.ms'):
+                            calibrated_files.append(os.path.join(products_path, file))
+    return calibrated_files
 
+# Specify the base directory
+base_directory = '../data/19B-053_2020_01_21_T08_37_05.970/'
 
-plotms(vis='selfcal_combine_pol_solint_48_6.tb',
-       xaxis='time',
-       yaxis='phase',
-       gridrows=3,
-       gridcols=3,
-       iteraxis='antenna',
-       plotrange=[0,0,-30,30],
-       coloraxis='corr',
-       titlefont=7,
-       xaxisfont=7,
-       yaxisfont=7,
-       showgui = True)
+# Get the list of calibrated files
+ms_file = find_calibrated_files(base_directory)
 
+for i in range (54):
 
-tclean(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',
-       imagename='initial',
-       stokes='I',
-       field='PER_FIELD_*, J0336+3218',
-       phasecenter='J2000 03:36:30.200000 +32.18.28.00000',
-       mosweight=True,
-       specmode='mfs',
-       deconvolver='mtmfs',
-       imsize=[5000], 
-       cell= '2.5arcsec', 
-       weighting='briggs', 
-       robust=0.5,
-       niter=300, 
-       threshold='1e-1', 
-       weighting="briggs",
-       gridder='awproject',
-       savemodel='modelcolumn',        
-       parallel=True,
-       psterm=True,
-       nterms=2,
-       rotatepastep=5.0,
-       interactive=False,
-       mask='../../masks/totat1.mask')
+        tclean(vis=ms_file,
+        field='PER_FIELD_'+str(i),       
+        imagename='./selfcal/PER_FIELD_'+str(i)+'_r1',
+        stokes='I',
+        specmode='mfs',
+        deconvolver='mtmfs',
+        imsize=[4096],
+        cell= '2.5arcsec',
+        weighting='briggs',
+        robust=0.5,
+        niter=700,
+        gridder='standard',
+        savemodel='modelcolumn',
+        usepointing=False,
+        rotatepastep=5.0,
+        nterms=2,
+        pbcor=True,
+        nsigma=5,
+        wbawp=True,
+        conjbeams=True,
+        pblimit=-0.01,
+        )
 
-
-tclean(vis='test.ms',
-       imagename='single_field_initial',
-       mosweight=True,
-       specmode='mfs',
-       deconvolver='hogbom',
-       imsize=[1296], 
-       cell= '2.5arcsec', 
-       weighting='briggs', 
-       robust=0.5,
-       niter=400, 
-       threshold='5e-4', 
-       interactive=True,
-       gridder='awproject',
-       savemodel='modelcolumn',
-       usepointing=False,
-       rotatepastep=5.0)
+        gaincal(vis=ms_file, field='PER_FIELD_'+str(i), caltable='./selfcal/pcal_1_f'+str(i),solint='int',calmode='p',gaintype='G')
+        applycal(vis=ms_file, field='PER_FIELD_'+str(i), gaintable='./selfcal/pcal_1_f'+str(i))
 
 
-tclean(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',
-       imagename='single_field_initial',
-       mosweight=True,
-       specmode='mfs',
-       deconvolver='hogbom',
-       imsize=[1296], 
-       cell= '2.5arcsec', 
-       weighting='briggs', 
-       robust=0.5,
-       niter=400, 
-       threshold='5e-4', 
-       interactive=True,
-       gridder='awproject',
-       savemodel='modelcolumn',
-       usepointing=False,
-       rotatepastep=5.0)
+        tclean(vis=ms_file,
+        field='PER_FIELD_'+str(i),       
+        imagename='./selfcal/PER_FIELD_'+str(i)+'_r2',
+        stokes='I',
+        specmode='mfs',
+        deconvolver='mtmfs',
+        imsize=[4096],
+        cell= '2.5arcsec',
+        weighting='briggs',
+        robust=0.5,
+        niter=1200,
+        gridder='standard',
+        savemodel='modelcolumn',
+        usepointing=False,
+        rotatepastep=5.0,
+        nterms=2,
+        pbcor=True,
+        nsigma=5,
+        wbawp=True,
+        conjbeams=True,
+        pblimit=-0.01,
+        )
 
-rmtables('pcal1')
-gaincal(vis='19B-053.sb37659292.eb37739287.58869.013119236115_calibrated_working.ms/',
-        caltable='mpcal1',
-        field='J0336+3218, PER_FIELD_*',
-        gaintype='G',
-        calmode='p',
-        solint='int')
-
-rmtables('bpcal1')
-bandpass(vis='19B-053.sb37659292.eb37739287.58869.013119236115_calibrated_working.ms/',
-        caltable='bpcal1',
-        gaintable='pcal2'
-        field='J0336+3218, PER_FIELD_*',
-        bandtype='B',
-        solint='int')
-
-applycal(vis='19B-053.sb37659292.eb37739287.58869.013119236115_calibrated_working.ms/',
-         field='J0336+3218, PER_FIELD_*',
-         gaintable=['mpcal1'])
+        gaincal(vis=ms_file, field='PER_FIELD_'+str(i), caltable='./selfcal/pcal_r2_f'+str(i),solint='int',calmode='p',gaintype='G')
+        bandpass(vis=ms_file, caltable='./selfcal/bpcal_r1_f'+str(i), gaintable='./selfcal/pcal_r2_f'+str(i),field='PER_FIELD_'+str(i),bandtype='B',solint='int')
+        applycal(vis=ms_file, field='PER_FIELD_'+str(i), gaintable=['./selfcal/pcal_r2_f'+str(i), './selfcal/bpcal_r1_f'+str(i)])
 
 
-flagmanager(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',mode='save',versionname='after_pcal1')
+        flagdata(vis=ms_file, datacolumn='residual', mode='tfcrop')
+
+        tclean(vis=ms_file,
+        field='PER_FIELD_'+str(i),       
+        imagename='./selfcal/PER_FIELD_'+str(i)+'_r3',
+        stokes='I',
+        specmode='mfs',
+        deconvolver='mtmfs',
+        imsize=[4096],
+        cell= '2.5arcsec',
+        weighting='briggs',
+        robust=0.5,
+        niter=1700,
+        gridder='standard',
+        savemodel='modelcolumn',
+        usepointing=False,
+        rotatepastep=5.0,
+        nterms=2,
+        pbcor=True,
+        nsigma=5,
+        wbawp=True,
+        conjbeams=True,
+        pblimit=-0.01,
+        )
 
 
-rmtables('pcal2')
-gaincal(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',
-        field='PER_FIELD_*,  J0336+3218',
-        caltable='pcal2',
-        gaintype='G',
-        refant='ea16',
-        calmode='p',
-        solint='96s', 
-        minsnr=3.0,
-        minblperant=6)
-
-applycal(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',
-         field='PER_FIELD_*,  J0336+3218',
-         gaintable=['pcal2'],
-         gainfield='',
-         calwt=False,
-         flagbackup=False)
-
-flagmanager(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',mode='save',versionname='after_pcal2')
+####################################################################################
 
 
-gaincal(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',
-        field='PER_FIELD_*, J0336+3218',
-        caltable='pcal3',
-        gaintype='G',
-        refant='ea16',
-        calmode='p',
-        solint='48s',
-        minsnr=3.0,
-        minblperant=6)
+tclean(vis=ms_file,
+field='J0336+3218',       
+imagename='./selfcal/J0336+3218_r1',
+stokes='I',
+specmode='mfs',
+deconvolver='mtmfs',
+imsize=[4096],
+cell= '2.5arcsec',
+weighting='briggs',
+robust=0.5,
+niter=700,
+gridder='standard',
+savemodel='modelcolumn',
+usepointing=False,
+rotatepastep=5.0,
+nterms=2,
+pbcor=True,
+nsigma=5,
+wbawp=True,
+conjbeams=True,
+pblimit=-0.01,
+)
 
-plotms(vis='pcal3',
-            xaxis='time',
-            yaxis='phase',
-            iteraxis='antenna',
-            plotrange=[0,0,-80,80],
-            gridrows=3,
-            gridcols=3)
-
-applycal(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',
-         field='PER_FIELD_*, J0336+3218', 
-         gaintable=['pcal3'],
-         gainfield='',
-         calwt=False,
-         flagbackup=False,)
-
-flagmanager(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',mode='save',versionname='after_pcal3')
-
-
-gaincal(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',
-        field='PER_FIELD_*, J0336+3218',
-        caltable='pcal4',
-        gaintype='G',
-        refant='ea16',
-        calmode='p',
-        solint='12s',
-        minsnr=3.0,
-        minblperant=6)
-
-plotms(vis='pcal4',
-            xaxis='time',
-            yaxis='phase',
-            iteraxis='antenna',
-            plotrange=[0,0,-180,180],
-            gridrows=3,
-            gridcols=3,
-            coloraxis='spw')
+gaincal(vis=ms_file, field='PER_FIELD_'+str(i), caltable='./selfcal/pcal_r1_J0336+3218',solint='int',calmode='p',gaintype='G')
+applycal(vis=ms_file, field='PER_FIELD_'+str(i), gaintable='./selfcal/pcal_r1_J0336+3218')
 
 
-applycal(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',
-         field='PER_FIELD_*, J0336+3218',
-         gaintable=['pcal4'],
-         gainfield='',
-         calwt=False,
-         flagbackup=False)
+tclean(vis=ms_file,
+field='J0336+3218',       
+imagename='./selfcal/J0336+3218_r2',
+stokes='I',
+specmode='mfs',
+deconvolver='mtmfs',
+imsize=[4096],
+cell= '2.5arcsec',
+weighting='briggs',
+robust=0.5,
+niter=1200,
+gridder='standard',
+savemodel='modelcolumn',
+usepointing=False,
+rotatepastep=5.0,
+nterms=2,
+pbcor=True,
+nsigma=5,
+wbawp=True,
+conjbeams=True,
+pblimit=-0.01,
+)
 
-flagmanager(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',mode='save',versionname='after_pcal4')
+gaincal(vis=ms_file, field='J0336+3218', caltable='./selfcal/pcal_r2_J0336+3218',solint='int',calmode='p',gaintype='G')
+bandpass(vis=ms_file, caltable='./selfcal/bpcal_r1_f'+str(i), gaintable='./selfcal/pcal_r2_J0336+3218',field='J0336+3218',bandtype='B',solint='int')
+applycal(vis=ms_file, field='J0336+3218', gaintable=['./selfcal/pcal_r2_J0336+3218', './selfcal/bpcal_r1_J0336+3218'])
 
 
-gaincal(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',
-        field='PER_FIELD_*, J0336+3218',
-        caltable='apcal',
-        gaintype='G',
-        refant='ea16',
-        calmode='ap',
-        solint='inf',
-        minsnr=3.0,
-        minblperant=6,
-#        uvrange='>50m', # may need to use to exclude extended emission
-        gaintable='pcal4',
-        solnorm=True)
+flagdata(vis=ms_file, datacolumn='residual', mode='tfcrop')
 
-
-applycal(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',
-         field='PER_FIELD_*, J0336+3218',
-         gaintable=['pcal4','apcal'],
-         gainfield='',
-         calwt=False,
-         flagbackup=False)
-
-flagmanager(vis='19B-053.sb37659292.eb37739287.58869.013119236115_flagged.ms/',mode='save',versionname='after_pcal5')
+tclean(vis=ms_file,
+field='J0336+3218',       
+imagename='./selfcal/J0336+3218_r3',
+stokes='I',
+specmode='mfs',
+deconvolver='mtmfs',
+imsize=[4096],
+cell= '2.5arcsec',
+weighting='briggs',
+robust=0.5,
+niter=1700,
+gridder='standard',
+savemodel='modelcolumn',
+usepointing=False,
+rotatepastep=5.0,
+nterms=2,
+pbcor=True,
+nsigma=5,
+wbawp=True,
+conjbeams=True,
+pblimit=-0.01,
+)
