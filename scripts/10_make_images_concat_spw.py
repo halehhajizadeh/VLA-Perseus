13c37ae2dcb80cbdca68b3dd226d85ff73bfb19c
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-07-09 12:00:04 -0600

    mosaic3

--- scripts/10_make_images_concat_spw.py
+++ scripts/10_make_images_concat_spw.py
@@ -18,7 +18,7 @@ spw = [
        16, 
        17
        ]
-phase_center = 'J2000 03:25:30.000000 +29.29.59.99999'
+phase_center = 'J2000 03:34:30.000000 +31.59.59.99999'
 
 def find_calibrated_files(base_directory):
     calibrated_files = []
@@ -36,7 +36,7 @@ def find_calibrated_files(base_directory):
     return calibrated_files
 
 # Specify the base directory
-mosaic_name = '03:25:30.000000_+29.29.59.99999/'
+mosaic_name = '03:34:30.000000_+31.59.59.99999/'
 base_directory = '../data/' + mosaic_name
 
 # Get the list of calibrated files
