72e6664bd5076792112622e7ee8a677d22f9bde3
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-07-12 00:31:07 -0600

    update  beam

--- scripts/spw-fits.py
+++ scripts/spw-fits.py
@@ -40,8 +40,8 @@ for data in specific_dirs:
 
         imsmooth(imagename=image_file,
                  targetres=True,
-                 major='55arcsec',
-                 minor='43arcsec',
+                 major='60arcsec',
+                 minor='40arcsec',
                  pa='0.0deg',
                  outfile=smo_file,
                  overwrite=True
