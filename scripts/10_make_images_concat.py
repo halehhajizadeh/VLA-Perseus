c59bfc8401c70a80dc588bf417b548e511f7c82a
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-07-17 00:56:42 -0600

    updat

--- scripts/10_make_images_concat.py
+++ scripts/10_make_images_concat.py
@@ -29,26 +29,20 @@ specific_dirs = '03:32:04.530001_+31.05.04.00000/data/'
 
 def find_calibrated_files(base_directory, specific_dirs):
     calibrated_files = []
-
     full_path = os.path.join(base_directory, specific_dirs)
     if os.path.exists(full_path) and os.path.isdir(full_path):
-        # Traverse all subdirectories within each specific directory
         for root, dirs, files in os.walk(full_path):
             products_path = os.path.join(root, 'products')
             if os.path.exists(products_path) and os.path.isdir(products_path):
                 for file in os.listdir(products_path):
                     if file.startswith('19B-053') and file.endswith('_calibrated.ms'):
                         calibrated_files.append(os.path.join(products_path, file))
-
     return calibrated_files
 
-# Specify the base directory
-base_directory = '../data/' 
+base_directory = '../data/'
 
-# Get the list of calibrated files
 ms_file_list = find_calibrated_files(base_directory, specific_dirs)
 
-# Print the list of calibrated files
 print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
 for file in ms_file_list:
     print(file)
