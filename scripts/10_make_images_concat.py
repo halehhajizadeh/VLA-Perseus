c89eb445ed65eb83e9d8b5b404d3c21343fde70c
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-07-30 14:36:32 -0600

    36

--- scripts/10_make_images_concat.py
+++ scripts/10_make_images_concat.py
@@ -20,13 +20,13 @@ spw = [
        ]
 
 # phase_center = 'J2000 03:32:04.530001 +31.05.04.00000'
-# phase_center = 'J2000 03:36:00.000000 +30.30.00.00001'
-phase_center = 'J2000 03:25:30.000000 +29.29.59.99999'
+phase_center = 'J2000 03:36:00.000000 +30.30.00.00001'
+# phase_center = 'J2000 03:25:30.000000 +29.29.59.99999'
 
 # specific_dirs = '03:32:04.530001_+31.05.04.00000/data/'
-# specific_dirs =  '03:36:00.000000_+30.30.00.00001/data/' 
+specific_dirs =  '03:36:00.000000_+30.30.00.00001/data/' 
 # specific_dirs =  '03:34:30.000000_+31.59.59.99999/data/'
-specific_dirs =  '03:25:30.000000_+29.29.59.99999/data/'
+# specific_dirs =  '03:25:30.000000_+29.29.59.99999/data/'
 # specific_dirs =  '03:23:30.000001_+31.30.00.00000/data/'
 
 def find_calibrated_files(base_directory, specific_dirs):
@@ -73,7 +73,7 @@ for stok in stokes1:
             tic = time.time()
             print(f"stokes: {stok}, s: {s}, channel: {channel} is started ...")
 
-            img_filename = os.path.join("../data/concat/", "03:25:30.000000_+29.29.59.9999/", "Images/img" + str(nit) + "/tclean/", "spw" + str(s) + '-' + str(channel) + "-2.5arcsec-nit" + str(nit) + "-" + str(thresh) + "-" + str(stok))
+            img_filename = os.path.join("../data/concat/", "03:36:00.000000_+30.30.00.00001/", "Images/img" + str(nit) + "/tclean/", "spw" + str(s) + '-' + str(channel) + "-2.5arcsec-nit" + str(nit) + "-" + str(thresh) + "-" + str(stok))
 
             tclean( vis=ms_file_list,
                     field="PER_FIELD_*",
