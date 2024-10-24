6f123938ed7770c4b7ed6c0f74df6d325cd08450
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-10-23 18:32:18 -0600

    55

--- scripts/11_smooth.py
+++ scripts/11_smooth.py
@@ -45,7 +45,7 @@ for stok in stokes:
             imsmooth(imagename = image_name,
                     targetres = True,
                     major = '55arcsec',
-                    minor ='50arcsec',
+                    minor ='55arcsec',
                     pa='0.0deg',
                     outfile = smo_image_name,
                     overwrite=True
