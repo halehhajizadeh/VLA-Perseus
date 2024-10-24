0f5e5c7b6fed66b5ec2fda4ee4b349bce5c5f72f
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-10-24 12:42:18 -0600

    fix

--- scripts/13_list_images.py
+++ scripts/13_list_images.py
@@ -6,27 +6,26 @@ import os
 # Define parameters
 nit = 5000
 thresh = '2e-4'
-spw = [9, 10, 11, 12, 13, 14, 15, 16, 17, 0, 1, 2, 3, 4, 5, 6, 7, 8]
+spw = [15, 16, 17, 0, 1, 2, 3, 4, 5, 6, 7, 8]
 channels = ['00~07', '08~15', '16~23', '24~31', '32~39', '40~47', '48~55', '56~63']
 stokes = ['I', 'Q', 'U']
 
 # Indices to replace with "empty_channel.fits" for each Stokes parameter
-
-# Drop indices for case #32
-# drop_indices = {
-#     'I': [41, 53, 80],
-#     'Q': [41, 45, 49, 80],
-#     'U': [41, 48, 49, 80, 95]
-# }
-
-# Drop indices for case #36
+#32
 drop_indices = {
-    'I': [41, 48, 80],
-    'Q': [41, 42, 48, 49],
-    'U': [41, 48, 49]
+    'I': [41, 53, 80],
+    'Q': [41, 45, 49, 80],
+    'U': [41, 48, 49, 80, 95]
 }
 
-# Drop indices for case #25
+# #36
+# drop_indices = {
+#     'I': [41, 48, 80],
+#     'Q': [41, 42, 48, 49],
+#     'U': [41, 48, 49]
+# }
+
+# #25
 # drop_indices = {
 #     'I': [41, 80],
 #     'Q': [41, 48, 49, 79, 80],
@@ -34,12 +33,11 @@ drop_indices = {
 # }
 
 # Define the specific directory you're using
-specific_dirs = '03:36:00.000000_+30.30.00.00001/'
-# other possible directories (commented for future use)
-# specific_dirs = '03:32:04.530001_+31.05.04.00000/'
-# specific_dirs = '03:34:30.000000_+31.59.59.99999/'
-# specific_dirs = '03:25:30.000000_+29.29.59.99999/'
-# specific_dirs = '03:23:30.000001_+31.30.00.00000/'
+specific_dirs = '03:32:04.530001_+31.05.04.00000/'  # As used in your previous scripts
+# specific_dirs =  '03:36:00.000000_+30.30.00.00001/'
+# specific_dirs =  '03:34:30.000000_+31.59.59.99999/'
+# specific_dirs =  '03:25:30.000000_+29.29.59.99999/'
+# specific_dirs =  '03:23:30.000001_+31.30.00.00000/'
 
 # Define the path for concatenation task
 path = f"../data/concat/{specific_dirs}"
@@ -50,7 +48,7 @@ file_list = [file for file in os.listdir(fits_path) if os.path.isfile(os.path.jo
 
 # Create a complete list of file paths
 file_list_total = [os.path.join(fits_path, file) for file in file_list]
-print(f"Total files found: {len(file_list_total)}")
+print(file_list_total)
 
 # Loop over stokes, SPWs, and channels
 for stok in stokes:
@@ -65,7 +63,6 @@ for stok in stokes:
             if file_index in drop_indices.get(stok, []):
                 # Replace with empty channel if the index is in the drop list
                 files_list.append(os.path.join(fits_path, "empty_channel.fits"))
-                print(f"Replacing index {file_index} for Stokes {stok} with empty channel.")
             elif file_name in file_list_total:
                 files_list.append(file_name)
             else:
@@ -74,6 +71,4 @@ for stok in stokes:
             file_index += 1  # Increment the file index
     
     # Save the list of files for the current Stokes parameter
-    np.savetxt(os.path.join(path, f"Images/img{nit}/Stokes{stok}.txt"), files_list, fmt='%s')
-
-    print(f"File list for Stokes {stok} saved with the appropriate empty channel replacements.")
+    np.savetxt(os.path.join(path, f"Images/img{nit}/", f"Stokes{stok}.txt"), files_list, fmt='%s')
