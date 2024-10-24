9bbc545c77c288327f85c18530e4af0ff36836e2
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-10-23 23:03:03 -0600

    update

--- scripts/14_cube.py
+++ scripts/14_cube.py
@@ -4,10 +4,10 @@
 # Fourth index is RA
 import sys
 sys.path.append('.')
-from configs import path, nit  # Removed threedigits
+from configs import path, nit
 import os
 import numpy as np
-from astropy.io import fits
+from casatools import image as ia  # CASA's image tool
 
 # Updated path with mosaic name (specific_dirs)
 specific_dirs = '03:32:04.530001_+31.05.04.00000/'  # As used in your previous scripts
@@ -16,26 +16,27 @@ specific_dirs = '03:32:04.530001_+31.05.04.00000/'  # As used in your previous s
 # specific_dirs =  '03:25:30.000000_+29.29.59.99999/'
 # specific_dirs =  '03:23:30.000001_+31.30.00.00000/'
 
-
-
 path = os.path.join(path, "concat", specific_dirs)
 
 stokes_list = ['I', 'Q', 'U']
 
-# Function to create an empty channel FITS file
+# Function to create an empty channel FITS file using CASA tools
 def create_empty_channel(fitsname):
     flagged_channel = os.path.join(path, f'Images/img{nit}/fits/empty_channel.fits')
-    
+
     # Remove existing empty channel file if it exists
     syscommand = f'rm -rf {flagged_channel}'
     os.system(syscommand)
-    
-    # Open the FITS file, set all data to NaN, and create the empty channel
-    hdul = fits.open(fitsname)
-    img_fits = hdul[0].data
-    img_fits[:] = np.nan
-    hdul[0].data = img_fits
-    hdul.writeto(flagged_channel)
+
+    # Open the image using CASA's image tool
+    ia.open(fitsname)
+    img_fits = ia.getchunk()  # Retrieve the image data
+    img_fits[:] = np.nan  # Set all data to NaN
+
+    # Create the empty channel FITS file
+    ia.putchunk(img_fits)
+    ia.tofits(flagged_channel, overwrite=True)
+    ia.close()
     return flagged_channel
 
 # Function to remove empty channels from the start of the file list
@@ -64,9 +65,9 @@ for stokes in stokes_list:
 
     # Adding the first channel to the list and initializing the cube
     inputfile = file_list[0]
-    hdulist = fits.open(inputfile)
-    img = hdulist[0].data
-    cube = np.copy(img[0, :, :, :])
+    ia.open(inputfile)
+    img = ia.getchunk()  # Get the data for the first channel
+    cube = np.copy(img[:, :, :, :])  # Copy it to initialize the cube
 
     # Create an empty channel FITS file
     create_empty_channel(inputfile)
@@ -74,18 +75,21 @@ for stokes in stokes_list:
 
     # Loop through the file list, appending data to the cube
     for filename in file_list:
-        with fits.open(filename) as hdulistCP:
-            imgCP = hdulistCP[0].data
-            if filename == os.path.join(path, f'Images/img{nit}/fits/empty_channel.fits'):
-                # Replace NaN values with 1e30 to avoid issues
-                imgCP[np.isnan(imgCP)] = 1e30
-            img2cube = np.copy(imgCP[0, :, :, :])
-            cube = np.append(cube, img2cube, axis=0)
+        ia.open(filename)
+        imgCP = ia.getchunk()
+        if filename == os.path.join(path, f'Images/img{nit}/fits/empty_channel.fits'):
+            # Replace NaN values with 1e30 to avoid issues
+            imgCP[np.isnan(imgCP)] = 1e30
+        img2cube = np.copy(imgCP[:, :, :, :])
+        cube = np.append(cube, img2cube, axis=0)
+        ia.close()
 
     print('For loop completed!')
 
     # Save the completed cube to the output FITS file
-    hdulist[0].data = cube
-    hdulist.writeto(cubename)
+    ia.open(inputfile)
+    ia.putchunk(cube)
+    ia.tofits(cubename, overwrite=True)
+    ia.close()
 
     print(f'Making cube for Stokes {stokes} is done!')
