dd5e50943bc42d6cf7f5538908192103e0352991
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2024-11-14 12:35:29 -0700

    update

--- scripts_24/1_make_images_ms.py
+++ scripts_24/1_make_images_ms.py
@@ -8,7 +8,7 @@ base_path = "../data/new/data/"
 # Define the common parameters for tclean
 def run_tclean(ms_file, img_filename, mosaic_name):
     thresh = '1e-4'
-    pblim = -0.0001
+    pblim = -0.001
     nit = 5000
 
     # Delete all files and directories matching imagename.*
@@ -32,9 +32,9 @@ def run_tclean(ms_file, img_filename, mosaic_name):
         intent="",
         datacolumn="corrected",
         imagename=img_filename,
-        imsize=[8192],
-        cell="1.5arcsec",
-        phasecenter="mosaic",
+        imsize=[4096],
+        cell="2arcsec",
+        phasecenter="",
         stokes='I',
         specmode="mfs",
         gridder="awproject",
