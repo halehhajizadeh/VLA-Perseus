e1fd60eaf6ae0cde9fb88a9b41ac95939ce9716b
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2025-01-24 15:10:58 -0700

    update

---
+++ scripts_24/1_make_single_image.py
@@ -0,0 +1,51 @@
+
+thresh = '9e-4'
+pblim = -0.001
+nit = 10
+ms_file = '../data/new/data/test/24A-376.sb45387559.eb45519359.60419.617289120375_calibrated.ms'
+img_filename = '../data/new/data/test/initialimage'
+phasecenter = 'J2000 03:26:24.057 +30.35.58.881'
+
+
+# Run tclean with specified parameters
+tclean(
+    vis=ms_file,
+    field="PER_FIELD_*",
+    timerange="",
+    spw="",
+    uvrange="",
+    antenna="",
+    observation="",
+    intent="",
+    datacolumn="corrected",
+    imagename=img_filename,
+    imsize=[4096],
+    cell="2.5arcsec",
+    phasecenter=phasecenter,  # Pass the phasecenter dynamically
+    stokes='I',
+    specmode="mfs",
+    gridder="awproject",
+    mosweight=True,
+    savemodel='modelcolumn',
+    cfcache=f'/dev/shm/{img_filename}.cf',
+    pblimit=pblim,
+    deconvolver="mtmfs",
+    pbcor=True,
+    weighting="briggs",
+    robust=0.5,
+    niter=nit,
+    gain=0.1,
+    # nsigma=3,
+    threshold=thresh,
+    cycleniter=200,
+    # psfcutoff=0.5,
+    cyclefactor=1,
+    parallel=True,
+    # psterm=True,
+    nterms=2,
+    rotatepastep=5.0,
+    interactive=False
+)
+
+
+
