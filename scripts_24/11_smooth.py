2252dc1215a8e3a6fc35b8b001eea2c06bddf7c4
Haleh Hajizadeh <haleh.hajizadeh94@gmail.com>
2026-01-08 22:30:58 -0700

    20,25

--- scripts_24/11_smooth.py
+++ scripts_24/11_smooth.py
@@ -81,8 +81,8 @@ for ms_path in ms_file_list:
 
             imsmooth(imagename=image_name,
                      targetres=True,
-                     major='22arcsec',
-                     minor='22arcsec',
+                     major='25arcsec',
+                     minor='20arcsec',
                      pa='0.0deg',
                      outfile=smo_image_name,
                      overwrite=True
